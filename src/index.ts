72fb536306757633bf2749190664b890e2272560
Collin Miller <colljmiller@gmail.com>
2021-12-02 15:59:57 -0600

    feat(plugin): exposing the plugin surface (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-import { success } from './lib/success';
+import { Context } from 'semantic-release';
+import { IPluginConfig, verify, success as successNx } from './lib';
 
-(() => {
-    success();
-})();
+let verified: boolean;
+
+export const verifyConditions = async (pluginConfig: IPluginConfig, context: Context) => {
+    await verify(pluginConfig, context);
+    verified = true;
+};
+
+export const success = async (pluginConfig: IPluginConfig, context: Context) => {
+    if (!verified) {
+        await verify(pluginConfig, context);
+        verified = true;
+    }
+
+    await successNx(pluginConfig, context);
+};
