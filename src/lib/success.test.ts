3f3e3092bdb1a18270511a962de75aeacfbcffaf
Collin Miller <colljmiller@gmail.com>
2021-12-07 09:26:14 -0600

    feat(success): added success logging (#13)

--- src/lib/success.test.ts
+++ src/lib/success.test.ts
@@ -2,9 +2,18 @@ import { describe, expect, it, jest } from '@jest/globals';
 import { success } from './success';
 
 describe('success', () => {
-    it('should log the success message', () => {
+    it('should log the success message with single artifact', () => {
         const logMock = jest.fn();
         success({ assets: [{ path: './some-path' }] }, { logger: { log: logMock, error: jest.fn() }, env: {} });
+        expect(logMock).toBeCalledTimes(1);
+        expect(logMock.mock.calls).toMatchSnapshot();
+    });
+    it('should log the success message with multiple artifacts', () => {
+        const logMock = jest.fn();
+        success(
+            { assets: [{ path: './some-path-again' }, { path: 'some-artifact' }] },
+            { logger: { log: logMock, error: jest.fn() }, env: {} },
+        );
         expect(logMock).toBeCalledTimes(1);
         expect(logMock.mock.calls).toMatchSnapshot();
     });
--- src/lib/success.ts
+++ src/lib/success.ts
@@ -1,7 +1,14 @@
 import { Context } from 'semantic-release';
 import { IPluginConfig } from './plugin';
 
-export const success = async (_pluginConfig: IPluginConfig, context: Context) => {
+export const success = async (pluginConfig: IPluginConfig, context: Context) => {
+    let stringifiedArtifacts: string;
     const { logger } = context;
-    logger.log('This is success.');
+    const artifacts = pluginConfig.assets.map((element) => element.path);
+    if (artifacts.length > 1) {
+        stringifiedArtifacts = `${artifacts.slice(0, -1).join(', ')} and ${artifacts[artifacts.length - 1]}`;
+    } else {
+        stringifiedArtifacts = artifacts.pop()!;
+    }
+    logger.log(`Successfully uploaded artifact(s): ${stringifiedArtifacts}.`);
 };
