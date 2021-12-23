cebe2e29450517606877fc9737f307d881115a84
Collin Miller <colljmiller@gmail.com>
2021-12-23 12:43:47 -0600

    fix: 🐛 verify (#23)
    removing file lookup since files may not exist until after exec

--- src/lib/verify.test.ts
+++ src/lib/verify.test.ts
@@ -1,25 +1,14 @@
-import { afterEach, describe, expect, jest, it } from '@jest/globals';
+import { describe, expect, jest, it } from '@jest/globals';
 import AggregateError from 'aggregate-error';
-import glob from 'tiny-glob';
 import { verify } from './verify';
 
-jest.mock('tiny-glob');
-
 describe('verify', () => {
-    const mockedGlob = glob as jest.Mocked<typeof glob>;
-    afterEach(() => {
-        (mockedGlob as jest.Mock).mockReset();
-    });
     it('should fail schema validation', async () => {
         await expect(
             verify({ assets: [] }, { logger: { log: jest.fn(), error: jest.fn() }, env: { key: 'value' } }),
         ).rejects.toThrowError(AggregateError);
     });
     it('should pass schema validation', async () => {
-        (mockedGlob as jest.Mock)
-            .mockImplementationOnce(() => ['dist/some-package-v1.tar.gz'])
-            .mockImplementationOnce(() => ['dist/other-package-v2.tar.gz']);
-        const logMock = jest.fn();
         await expect(
             verify(
                 {
@@ -35,22 +24,13 @@ describe('verify', () => {
                     ],
                 },
                 {
-                    logger: { log: logMock, error: jest.fn() },
+                    logger: { log: jest.fn(), error: jest.fn() },
                     env: { NX_HOST: 'https://some-host', NX_REPO: 'some-repo' },
                 },
             ),
         ).resolves.toBeUndefined();
-        expect(mockedGlob).toHaveBeenCalledTimes(2);
-        expect(mockedGlob).toHaveBeenNthCalledWith(1, 'dist/some-package-v*.tar.gz', { filesOnly: true });
-        expect(mockedGlob).toHaveBeenNthCalledWith(2, 'dist/other-package-v*.tar.gz', { filesOnly: true });
-        expect(logMock).toBeCalled();
-        expect(logMock.mock.calls).toMatchSnapshot();
     });
     it('should fail schema validation for an additional asset map', async () => {
-        (mockedGlob as jest.Mock)
-            .mockImplementationOnce(() => ['dist2/some-package-v1.tar.gz'])
-            .mockImplementationOnce(() => ['dist2/other-package-v2.tar.gz']);
-        const logMock = jest.fn();
         await expect(
             verify(
                 {
@@ -60,13 +40,8 @@ describe('verify', () => {
                         { path: 'dist2/other-package-v*.tar.gz', booger: true },
                     ],
                 },
-                { logger: { log: logMock, error: jest.fn() }, env: { key: 'value' } },
+                { logger: { log: jest.fn(), error: jest.fn() }, env: { key: 'value' } },
             ),
         ).rejects.toThrowError(AggregateError);
-        expect(mockedGlob).toHaveBeenCalledTimes(2);
-        expect(mockedGlob).toHaveBeenNthCalledWith(1, 'dist2/some-package-v*.tar.gz', { filesOnly: true });
-        expect(mockedGlob).toHaveBeenNthCalledWith(2, 'dist2/other-package-v*.tar.gz', { filesOnly: true });
-        expect(logMock).toBeCalled();
-        expect(logMock.mock.calls).toMatchSnapshot();
     });
 });
--- src/lib/verify.ts
+++ src/lib/verify.ts
@@ -1,8 +1,7 @@
 import Ajv from 'ajv';
 import AggregateError from 'aggregate-error';
-import glob from 'tiny-glob';
 import { Context } from 'semantic-release';
-import { IPluginAssets, IPluginConfig, PluginSchema, resolveOptions } from './plugin';
+import { IPluginConfig, PluginSchema, resolveOptions } from './plugin';
 
 export const isValidUrl = (url: string | undefined) => {
     try {
@@ -18,27 +17,11 @@ export const isValidUrl = (url: string | undefined) => {
     return true;
 };
 
-export const verifyAssets = (assets: Array<IPluginAssets>, context: Context): string[] => {
-    const errors: string[] = [];
-    assets.forEach(async (element) => {
-        const files = await glob(element.path, { filesOnly: true });
-        if (files.length === 0) {
-            errors.push(`No asset was found at path ${element.path}`);
-        } else if (files.length > 1) {
-            errors.push(`Only one file supported per asset declaration. Files found ${files.join(' ')}`);
-        } else {
-            context.logger.log(`Found file ${files.join('')} from path ${element.path}`);
-        }
-    });
-    return errors;
-};
-
 /**
  * A method to verify the inputs
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const verify = async (pluginConfig: IPluginConfig, context: Context) => {
-    // const { logger } = context;
     const errors: string[] = [];
 
     const ajv = new Ajv({ allErrors: true });
@@ -48,7 +31,6 @@ export const verify = async (pluginConfig: IPluginConfig, context: Context) => {
         ajv.errors?.forEach((element) => errors.push(element.message || 'validation error'));
     }
     // verify a valid file is found at each asset path
-    errors.push(...verifyAssets(pluginConfig.assets, context));
     const options = resolveOptions(pluginConfig, context);
     if (!isValidUrl(options.nexusHost)) {
         errors.push(`The nexus host provided (${options.nexusHost}) is invalid.`);
